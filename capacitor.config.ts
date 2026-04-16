9159a040528138807e7bed30d1e56709d7fdc331
Tebias-cloud <esteban.vidal.valencia@gmail.com>
2026-04-16 03:46:30 -0400

    Habilitar HTTP Cleartext y CapacitorHttp para bypass de CORS

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,12 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'ridertv.app',
   appName: 'Rider TV',
-  webDir: 'out'
+  webDir: 'out',
+  plugins: {
+    CapacitorHttp: {
+      enabled: true
+    }
+  }
 };
 
 export default config;
